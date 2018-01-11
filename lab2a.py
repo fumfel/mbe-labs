2ea04419f2601ed555e92c49193369826fb3c7e0
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-11 09:26:44 +0100

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -3,6 +3,7 @@ import struct
 
 RA_FROM_CONCATENATE_FIRST_CHARS = 0x080487E6
 
+
 for c in range(0x41, 0x5F):
 	char_endline = chr(c+10).lower()
 	if c is 0x4d:
@@ -15,5 +16,13 @@ for c in range(0x41, 0x5F):
 		print("\x04\x08" * 8)
 	elif c >= 0x51:
 		print ((chr(c)*16 + chr(c+10)))
+	elif c is 0x4b:
+		print("\xa8\xf5" * 8)
+	elif c is 0x4c:
+		print("\xff\xbf" * 8)
+	elif c is 0x5e:
+		print("\x83\xca" * 8)
+	elif c is 0x5d:
+		print("\xe3\xb7" * 8)		
 	else:
 		print(chr(c)*16 + char_endline)
