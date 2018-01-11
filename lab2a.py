9a7c5e6ca31f3fa3c6184945857fc7a082c7871a
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-11 09:53:11 +0100

    Update lab2a.py

--- lab2a.py
+++ lab2a.py
@@ -14,15 +14,15 @@ for c in range(0x41, 0x5F):
 		print("\xe6\x87\x04\x08" * 2 + "\xe6\x87" * 4)
 	elif c is 0x50:
 		print("\x04\x08" * 8)
-	elif c >= 0x51:
-		print ((chr(c)*16 + chr(c+10)))
 	elif c is 0x4b:
 		print("\xa8\xf5" * 8)
 	elif c is 0x4c:
 		print("\xff\xbf" * 8)
 	elif c is 0x5e:
-		print("\x83\xca" * 8)
+		print("\xe3\xb7" * 8)
 	elif c is 0x5d:
-		print("\xe3\xb7" * 8)		
+		print("\x83\xca" * 8)
+	elif c >= 0x51:
+		print ((chr(c)*16 + chr(c+10)))		
 	else:
 		print(chr(c)*16 + char_endline)
